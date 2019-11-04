39a64aa4d3982f198cdcf78b86ad207082d60df8
John Shenk <john.shenk@rackspace.com>
2019-11-04 08:20:35 -0600

    mods lambda handler in tf

--- api/terraform/main.tf
+++ api/terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_lambda_function" "beanieboo_server_lambda" {
   filename      = "lambda.zip"
   function_name = "beaniebooserverlambda"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
-  handler       = "api/serverlambda.handler"
+  handler       = "api/app/serverlambda.handler"
   source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
 
   runtime = "nodejs8.10"
