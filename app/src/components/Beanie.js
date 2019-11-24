6fd6e3780fb768da0a6d7a4425963e26fc9fc42f
John Shenk <john.shenk@rackspace.com>
2019-11-24 10:48:58 -0600

    css on beanire

--- app/src/components/Beanie.js
+++ app/src/components/Beanie.js
@@ -5,7 +5,7 @@ import '../css/beanie.css';
 
 const Beanie = ({beanie}) => {
   const userState = useContext(UserContext);
-  const disabled = userState.user.admin ? false : true; 
+  const disabled = userState.user.admin ? false : true;
   const [beanieValue, setBeanieValue] = useState(beanie);
   const token = userState.user.token;
 
@@ -58,43 +58,43 @@ const Beanie = ({beanie}) => {
 
   return (
     <div className='beanie'>
-      <label htmlFor='name'>Name:
-        <input type='text' name='name' defaultValue={beanieValue.name || ''} disabled={disabled} onChange={handleChange} />
-      </label>
-      <label htmlFor='animal'>Animal:
-        <input type='text' name='animal' defaultValue={beanieValue.animal || ''} disabled={disabled} onChange={handleChange} />
-      </label>
-      <label htmlFor='birthday'>Birthday:
-        <input type='date' name='birthday' data-testid='birthday' defaultValue={birthday} disabled={disabled} onChange={handleChange}/>
-      </label>
-      <label htmlFor='exclusiveTo'>Exclusive To:
-        <input type='text' name='exclusiveTo' defaultValue={beanieValue.exclusiveTo || ''} disabled={disabled} onChange={handleChange} />
-      </label>
-      <label htmlFor='family'>Family:
-        <select name='family' defaultValue={beanieValue.family} disabled={disabled} onChange={handleChange}>
-          <option value='Beanie Babies'>Beanie Babies</option>
-          <option value='Beanie Babies 2.0'>Beanie Babies 2.0</option>
-          <option value='Beanie Boos'>Beanie Boos</option>
-        </select>
-      </label>
-      <label htmlFor='height'>Height:
-        <input type='text' name='height' defaultValue={beanieValue.height || ''} disabled={disabled} onChange={handleChange} />
-      </label>
-      <label htmlFor='length'>Length:
-        <input type='text' name='length' defaultValue={beanieValue.length || ''} disabled={disabled} onChange={handleChange} />
-      </label>
-      <label htmlFor='number'>Number:
-        <input type='text' name='number' defaultValue={beanieValue.number} disabled={disabled} onChange={handleChange}  />
-      </label>
-      <label htmlFor='introDate'>Intro Date:
-        <input type='date' name='introDate' defaultValue={introDate} disabled={disabled} onChange={handleChange} />
-      </label>
-      <label htmlFor='retireDate'>Retire Date:
-        <input type='date' name='retireDate' defaultValue={retireDate} disabled={disabled} onChange={handleChange} />
-      </label>
-      <label htmlFor='image'>Image URL:
-        <input type='text' name='image' defaultValue={beanieValue.image || ''} disabled={disabled} onChange={handleChange} />
-      </label>
+      <label htmlFor='name'>Name:</label>
+      <input type='text' name='name' defaultValue={beanieValue.name || ''} disabled={disabled} onChange={handleChange} />
+
+      <label htmlFor='animal'>Animal:</label>
+      <input type='text' name='animal' defaultValue={beanieValue.animal || ''} disabled={disabled} onChange={handleChange} />
+
+      <label htmlFor='birthday'>Birthday:</label>
+      <input type='date' name='birthday' data-testid='birthday' defaultValue={birthday} disabled={disabled} onChange={handleChange}/>
+
+      <label htmlFor='exclusiveTo'>Exclusive To:</label>
+      <input type='text' name='exclusiveTo' defaultValue={beanieValue.exclusiveTo || ''} disabled={disabled} onChange={handleChange} />
+
+      <label htmlFor='family'>Family:</label>
+      <select name='family' defaultValue={beanieValue.family} disabled={disabled} onChange={handleChange}>
+        <option value='Beanie Babies'>Beanie Babies</option>
+        <option value='Beanie Babies 2.0'>Beanie Babies 2.0</option>
+        <option value='Beanie Boos'>Beanie Boos</option>
+      </select>
+
+      <label htmlFor='height'>Height:</label>
+      <input type='text' name='height' defaultValue={beanieValue.height || ''} disabled={disabled} onChange={handleChange} />
+
+      <label htmlFor='length'>Length:</label>
+      <input type='text' name='length' defaultValue={beanieValue.length || ''} disabled={disabled} onChange={handleChange} />
+
+      <label htmlFor='number'>Number:</label>
+      <input type='text' name='number' defaultValue={beanieValue.number} disabled={disabled} onChange={handleChange} />
+
+      <label htmlFor='introDate'>Intro Date:</label>
+      <input type='date' name='introDate' defaultValue={introDate} disabled={disabled} onChange={handleChange} />
+
+      <label htmlFor='retireDate'>Retire Date:</label>
+      <input type='date' name='retireDate' defaultValue={retireDate} disabled={disabled} onChange={handleChange} />
+
+      <label htmlFor='image'>Image URL:</label>
+      <input type='text' name='image' defaultValue={beanieValue.image || ''} disabled={disabled} onChange={handleChange} />
+      
       <div className='image'>
         <img src={beanieValue.image} alt={beanieValue.name}/>
       </div>
