3c294c282bfbc8ff726e3e38547f4d338802abe9
John Shenk <john_shenk@hotmail.com>
2020-11-07 14:33:07 -0600

    adds crude pdf

--- appv2/src/components/Beanies.js
+++ appv2/src/components/Beanies.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useQuery, useInfiniteQuery } from 'react-query';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import { PDFDownloadLink } from '@react-pdf/renderer';
 import * as api from '../api';
-import * as hooks from '../hooks';
+import Pdf from './Pdf';
 import Context from '../Context';
 import Loading from './Loading';
 import Error from './Error';
@@ -10,15 +11,23 @@ import '../css/beanies.css';
 
 const numberOfResponsesFromAPI = 100;
 
-const BeanieSummary = ({ beanie, handleClick, handleDrag }) => {
-  return <tr className='beanieSummary' onClick={() => handleClick(beanie, 'beanie')} draggable={true} onDragStart={() => handleDrag(beanie)}>
-    <td>{beanie.name}<div className='subtext'>{beanie.animal}</div></td>
-    <td>{beanie.thumbnail ? <img src={beanie.thumbnail} alt={beanie.name} /> : null}</td>
+const PdfLink = ({ beanies, token }) => <React.Fragment>
+    <PDFDownloadLink document={<Pdf beanies={beanies} title='Beanies' token={token}/>} fileName="beanies.pdf">
+    {({ blob, url, loading, error }) => (loading ? 'Loading pdf...' : <button>Download pdf</button>)}
+    </PDFDownloadLink>
+  </React.Fragment>;
+
+const BeanieSummary = ({ beanie, handleClick, handleDrag, pdfBeanieNames, handleChange }) => {
+  return <tr className='beanieSummary' draggable={true} onDragStart={() => handleDrag(beanie)}>
+    <td><input type='checkbox' checked={pdfBeanieNames.includes(beanie.name) ? 'checked' : ''} onChange={handleChange} value={beanie.name}/></td>
+    <td onClick={() => handleClick(beanie, 'beanie')}>{beanie.name}<div className='subtext'>{beanie.animal}</div></td>
+    <td onClick={() => handleClick(beanie, 'beanie')}>{beanie.thumbnail ? <img src={beanie.thumbnail} alt={beanie.name} /> : null}</td>
   </tr>;
 };
 
 const Beanies = ({ handleClick, handleDrag }) => {
   const { state } = React.useContext(Context);
+  const [pdf, setPdf] = React.useState({ ready: false, beanies: [] });
   const getFamily = (family, startKey) => api.family(state.family, startKey);
 
   const { isLoading, error, data, fetchMore, canFetchMore } = useInfiniteQuery('family', getFamily, {
@@ -32,15 +41,42 @@ const Beanies = ({ handleClick, handleDrag }) => {
   if (error) return <Error msg={error} />;
   const dataLength = data ? _.sum(_.map(data, group => group.length)) : 0;
 
-  const renderBeaniesSummary = () => data.map(group => group.map(beanie => <BeanieSummary key={beanie.name} beanie={beanie} handleClick={handleClick} handleDrag={handleDrag} />));
+  const handlePdfBeanieChange = (e) => {
+    const name = e.target.value;
+    const beanies = pdf.beanies;
+    if (beanies.includes(name)) {
+      _.remove(pdf.beanies, name);
+    } else {
+      beanies.push(name);
+    }
+    setPdf({ ready: false, beanies});
+  };
+
+  const renderBeaniesSummary = () => data.map(group => group.map(beanie => <BeanieSummary key={beanie.name} beanie={beanie} handleClick={handleClick} handleDrag={handleDrag} pdfBeanieNames={pdf.beanies} handleChange={handlePdfBeanieChange}/>));
+
+  const createPdf = () => {
+    return Promise.all(pdf.beanies.map(beanie => api.get(state.user.token, state.family, beanie)))
+      .then(pdfBeanies => setPdf({ ready: true, beanies: pdfBeanies }));
+  };
+
+  const renderPdf = () => {
+    if (pdf.ready) return <React.Fragment>
+      <PdfLink token={state.user.token} beanies={pdf.beanies} />
+      <button onClick={() => setPdf({ ready: false, beanies: [] })}>Cancel pdf</button>
+    </React.Fragment>;
+    return <React.Fragment>
+      <button onClick={createPdf}>Create pdf</button>
+    </React.Fragment>;
+  };
 
   return <div className='beaniesTable'>
+    {renderPdf()}
     <InfiniteScroll
       next={() => fetchMore()}
       hasMore={canFetchMore}
       dataLength={dataLength}
       loader={<Loading />}
-      height={window.innerWidth * .8}
+      height={window.innerHeight * .75}
     >
       <table className='beaniesSummary'>
         <thead><tr><td colSpan={2}><div className='subtext'>Click to view or drag to add</div></td></tr></thead>
