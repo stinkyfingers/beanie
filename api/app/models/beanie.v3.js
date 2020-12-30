91009d1c04c60d14f6d356928222adb533d81e67
John Shenk <john_shenk@hotmail.com>
2020-12-30 07:57:15 -0600

    v3 add images

--- api/app/models/beanie.v3.js
+++ api/app/models/beanie.v3.js
@@ -1,20 +1,37 @@
 'use strict';
 const redis = require('redis');
+const AWS = require('aws-sdk');
 const utilities = require('./utilities');
-const config = require('../config')
+const config = require('../config');
 
+// AWS
+const region = 'us-west-1';
+const imageBucket = 'beaniedata.john-shenk.com';
+AWS.config.update({region: region});
+if (process.env.NODE_ENV === 'live') {
+  AWS.config.credentials = new AWS.SharedIniFileCredentials({profile: 'jds'}); // run locally with live db
+}
+const s3 = new AWS.S3();
+
+// Redis
 const db = 0;
 const client = redis.createClient(config.redisUrl(db));
+
+const imageKey = (family, name) => `${name}.${family}`;
 
 const create = (beanie) => {
   return utilities.getBase64ImageDataAndThumbnail(beanie)
     .then(resp => {
-      if (!resp) return beanie;
+      if (!resp) return null;
       beanie.thumbnail = resp.thumbnail;
-      beanie.image = resp.base64String;
-      return beanie;
+      const image = resp.base64String
+      return s3.upload({
+        Bucket: imageBucket,
+        Key: imageKey(beanie.family, beanie.name),
+        Body: Buffer.from(image)
+      }).promise();
     })
-    .then(beanie => new Promise((res, rej) => {
+    .then(() => new Promise((res, rej) => {
       return client.set(`${beanie.family}:${beanie.name}`, JSON.stringify(beanie), (err, resp) => {
         if (err) rej(err);
         res(resp);
@@ -23,10 +40,18 @@ const create = (beanie) => {
 };
 
 const get = (family, name) => {
-  return new Promise((res, rej) => {
-    return client.get(`${family}:${name}`, (err, resp) => {
-      if (err) rej(err);
-      res(JSON.parse(resp));
+  return s3.getObject({
+    Bucket: imageBucket,
+    Key: imageKey(family, name)
+  }).promise()
+  .then(object => {
+    return new Promise((res, rej) => {
+      return client.get(`${family}:${name}`, (err, resp) => {
+        if (err) rej(err);
+        const beanie = JSON.parse(resp);
+        beanie.image = object.Body.toString();
+        res(beanie);
+      });
     });
   });
 };
@@ -39,7 +64,12 @@ const family = (family) => {
 
       return Promise.all(resp[1].sort().map(key => {
         const [family, name] = key.split(':');
-        return get(family, name);
+        return new Promise((res, rej) => {
+          return client.get(`${family}:${name}`, (err, resp) => {
+            if (err) rej(err);
+            res(JSON.parse(resp));
+          });
+        });
       }))
         .then(res);
     });
@@ -50,10 +80,15 @@ const remove = (family, name) => {
   return new Promise((res, rej) => {
     return client.del(`${family}:${name}`, (err, resp) => {
       if (err) rej(err);
-      res(resp);
+      res(s3.deleteObject({
+        Bucket: imageBucket,
+        Key: imageKey(family, name)
+      }).promise());
     });
   });
 };
+
+
 
 module.exports = {
   create,
